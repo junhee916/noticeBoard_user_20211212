999032390c4a73aa28dad1b4337efb2e88738160
junhee916 <junhee916@naver.com>
2021-12-12 17:20:08 +0900

    noticeBoard_user restapi node connect mongodb

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const userRouter = require('./route/user')
+const connectDB = require('./config/database')
+connectDB()
+// middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
+app.use(morgan('dev'))
+// require route
+app.use('/user', userRouter)
+const PORT = process.env.PORT || 7000
+app.listen(PORT, console.log("connected server..."))
